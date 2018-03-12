f9deeab48b594a6da72e53d09d7b0838356681e6
Jeffkungu <Jeffkungu@gmail.com>
2018-03-12 17:26:13 +0300

    [Feature] adds contract to describe the communication between View and Presenter.

---
+++ app/src/main/java/com/andela/art/lostasset/LostAssetContract.java
@@ -0,0 +1,21 @@
+package com.andela.art.lostasset;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jeffkungu on 12/03/2018.
+ */
+
+public interface LostAssetContract {
+
+    interface LostAssetModel {
+        ArrayList<String> fetchCohorts();
+        ArrayList<String> fetchAssets();
+
+    }
+
+    interface LostAssetView {
+        void showCohorts(ArrayList<String> cohorts);
+        void showAssets(ArrayList<String> assets);
+    }
+}
