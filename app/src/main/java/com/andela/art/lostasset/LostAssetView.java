02323694db5000a88fc9a502f1f0c6026897471c
Jeffkungu <Jeffkungu@gmail.com>
2018-03-12 18:02:07 +0300

    remove LostAssetView.java file

--- app/src/main/java/com/andela/art/lostasset/LostAssetView.java
+++
@@ -1,9 +0,0 @@
-package com.andela.art.lostasset;
-
-
-import java.util.ArrayList;
-
-public interface LostAssetView {
-    void showCohorts(ArrayList<String> cohorts);
-    void showAssets(ArrayList<String> assets);
-}
